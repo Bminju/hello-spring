2e99dfd9ed199d7d78e7aed71507eb2051d6cbba
Bminju <minjuuuz@gmail.com>
2021-05-11 23:26:40 +0900

    mvc-api

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -1,9 +1,11 @@
 package hello.hellospring.controller;
 
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -16,7 +18,36 @@ public class HelloController {
 
     @GetMapping("hello-mvc")
     public String helloMvc(@RequestParam(value = "name", required = false) String name, Model model) {
-        model.addAttribute("name",name);
+        model.addAttribute("name","name");
         return "hello-template";
+    }
+
+    // API 방식
+    @GetMapping("hello-string")
+    @ResponseBody //http의 body부에 (내용)을 직접 넣어주겠다. view template가 필요없음.
+    public String helloString(@RequestParam("name") String name) {
+        return "hello " + name; // "hello spring"
+    }
+
+    // json 방식 - 기본 설정
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    static class Hello {
+        private String name;
+
+        //getter and setter method 단축키 : ALT + insert
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
